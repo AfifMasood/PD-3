b8b774d2af43dfc70418c2064f4f238e4e2923f8
AfifMasood 180 <144409057+AfifMasood@users.noreply.github.com>
2023-09-23 00:56:32 +0500

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+main()
+{
+ int input;
+ int sum=0;
+ cout<<"Integer 1: ";
+ cin>> input;
+ sum=input+sum;
+ cout<<"Integer 2: ";
+ cin>> input;
+ sum=input+sum;
+ cout<<"Integer 3: ";
+ cin>> input;
+ sum=input+sum;
+ cout<<"Integer 5: ";
+ cin>> input;
+ sum=input+sum;
+ cout<<"Integer 5: ";
+ cin>> input;
+ sum=input+sum;
+ cout<<"The sum is: "<<sum;
+}
+ 
+ 
